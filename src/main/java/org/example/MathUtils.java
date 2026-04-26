d01631f616324f95d6185fa9268d5a21f1a27de7
25020078-TranManhDung <25020078@vnu.edu.vn>
2026-04-26 10:42:15 +0700

    Sửa lại format MathUtils và MathUtilsTest chuẩn google

--- src/main/java/org/example/MathUtils.java
+++ src/main/java/org/example/MathUtils.java
@@ -3,22 +3,22 @@ package org.example;
 import java.nio.file.Path;
 
 public class MathUtils {
-    public static int max(int a, int b) {
-        if (a >= b) return a;
-        return b;
-    }
+  public static int max(int a, int b) {
+    if (a >= b) return a;
+    return b;
+  }
 
-    public static int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Divider must not be zero");
-        }
-        return a / b;
+  public static int divide(int a, int b) {
+    if (b == 0) {
+      throw new IllegalArgumentException("Divider must not be zero");
     }
+    return a / b;
+  }
 
-    public static String getFilePath(String folder, String fileName) {
-        //Sử dụng Path.of để tự động chọn dấu gạch chéo đúng theo OS
-        return Path.of(folder, fileName).toString();
-    }
+  public static String getFilePath(String folder, String fileName) {
+    //Sử dụng Path.of để tự động chọn dấu gạch chéo đúng theo OS
+    return Path.of(folder, fileName).toString();
+  }
 }
 
 
